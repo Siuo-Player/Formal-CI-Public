af2df9a5d3e8e68af78c2b5e4422531f18b2dc6a
Rafael Bernardo <player1rgb@gmail.com>
2026-09-04 23:55:51 +0100

    formal: wire P16 reachability into current root module

--- formal/lean/FiniteBridge.lean
+++ formal/lean/FiniteBridge.lean
@@ -6,6 +6,7 @@ import FiniteBridge.DependencyClosure
 import FiniteBridge.SemanticDependency
 import FiniteBridge.SemanticLocality
 import FiniteBridge.SemanticExactness
+import FiniteBridge.ChainReachability
 import FiniteBridge.MultiSeedExactness
 
 namespace FiniteBridge
